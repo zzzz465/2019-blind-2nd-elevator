d1d87a70db5b8e400d467333b28fdacd50e70762
madeline <zzzz465@naver.com>
2021-02-11 15:44:40 +0900

    좀 더 코드를 보기쉽게 리팩토링함

--- solution/src/index.ts
+++ solution/src/index.ts
@@ -56,216 +56,157 @@ class CallDB {
     }
 }
 
-const MAX_PASSENGERS = 8
+const MAX_PASSENGERS_CAPACITY = 8
 
 class Elevator implements iElevator {
     direction?: 'UP' | 'DOWN' | null
-    constructor(public id: number, public floor: number, public passengers: Call[], public status: ElevatorStatus, public maxHeight: number, public waitingCalls: CallDB) { }
+    command: CommandString
+    constructor(public id: number, public floor: number, public passengers: Call[], public status: ElevatorStatus, public maxHeight: number, public waitingCalls: CallDB) {
+        this.command = 'STOP'
+    }
 
     calculate(): Command {
-        const result: Command = {
-            command: 'STOP',
-            elevator_id: this.id,
-            call_ids: []
-        }
         switch (this.status) {
-            case 'UPWARD': {
-                if (this.floor == this.maxHeight) {
-                    result.command = 'STOP'
-                } else {
-                    if (this.PassengerExitThisFloor().length > 0) {
-                        result.command = 'STOP'
-                    } else if (this.PassengerEnterOnThisFloorWithSameDirection().length > 0) {
-                        result.command = 'STOP'
-                    } else {
-                        const { down, up } = this.callsLookUp(false)
-                        if (up > 0)
-                            result.command = 'UP'
-                        else
-                            result.command = 'STOP'
-                    }
-                }
-            } break
-
-            case 'DOWNWARD': {
-                if (this.floor === 1) { // FLOOR
-                    result.command = 'STOP'
-                } else {
-                    if (this.PassengerExitThisFloor().length > 0) {
-                        result.command = 'STOP'
-                    } else if (this.PassengerEnterOnThisFloorWithSameDirection().length > 0) {
-                        result.command = 'STOP'
-                    } else {
-                        const { down } = this.callsLookUp(false)
-                        if (down > 0)
-                            result.command = 'DOWN'
-                        else
-                            result.command = 'STOP'
-                    }
-                }
-            } break
-
             case 'STOPPED': {
-                const exitPassengers = this.PassengerExitThisFloor()
-                const enterPassengers = this.PassengerEnterOnThisFloorWithSameDirection()
-                if (exitPassengers.length > 0 || enterPassengers.length > 0) {
-                    result.command = 'OPEN'
-                    result.call_ids = exitPassengers.map(d => d.id)
-                } else {
-                    if (this.passengers.length > 0) {
-                        const direction = this.ElevatorDirections()
-                        if (direction) {
-                            result.command = direction
-                        } else { // 방향이 있어야하는데 없다???
-                            throw new Error()
+                if (this.exitExists()) { // case 1
+                    return this.open()
+                } else if (this.enterExists() && !this.full()) {
+                    return this.open()
+                } else { // case 2
+                    if (!this.empty()) { // 사람이 있을경우
+                        const direction = this.getDirection()
+                        switch (direction) {
+                            case 'UP':
+                                return this.up()
+
+                            case 'DOWN':
+                                return this.down()
+
+                            case null:
+                                throw new Error()
                         }
                     } else {
-                        const waitingThisFloor = this.WaitingCallsEnterThisFloor()
-                        if (waitingThisFloor.length > 0) {
-                            result.command = 'OPEN'
-                        } else {
-                            const { down, up } = this.callsLookUp(true)
-                            if (up > 0 || down > 0) {
-                                if (up >= down) {
-                                    result.command = 'UP'
-                                } else {
-                                    result.command = 'DOWN'
-                                }
+                        const { down, up } = this.lookup()
+
+                        if (up > 0 || down > 0) {
+                            if (up > down) {
+                                return this.up()
+                            } else if (down > up) {
+                                return this.down()
                             } else {
-                                result.command = 'STOP'
+                                // 동일할경우, 끝 지점(가장 밑, 가장 위)이 더 가까운 방향을 선택
+                                if (this.floor >= Math.floor(MAX_HEIGHT / 2))
+                                    return this.up()
+                                else
+                                    return this.down()
                             }
+                        } else {
+                            return this.stop()
                         }
                     }
                 }
             } break
 
             case 'OPENED': {
-                const exitPassengers = this.PassengerExitThisFloor()
-
-                if (exitPassengers.length > 0) {
-                    result.command = 'EXIT'
-                    result.call_ids = exitPassengers.map(d => d.id)
+                if (this.exitExists()) {
+                    return this.exit()
+                } else if (this.enterExists() && !this.full()) {
+                    return this.enter()
                 } else {
-                    const { up, down } = this.callsLookUp(true)
+                    return this.close()
+                }
+            } break
 
-                    if (up > 0 || down > 0) {
-                        let calls = this.WaitingCallsEnterThisFloor()
-                        if (calls.length > 0) {
-                            result.command = 'ENTER'
-                            if (up > down)
-                                calls = calls.filter(d => d.end - d.start > 0)
-                            else
-                                calls = calls.filter(d => d.end - d.start < 0)
+            case 'UPWARD':
+            case 'DOWNWARD': {
+                if (this.exitExists()) {
+                    return this.stop()
+                } else if (this.enterExists()) {
+                    if (this.full()) {
+                        return this.keepGoing()
+                    } else {
+                        return this.stop()
+                    }
+                } else if (this.floor === 1 || this.floor === MAX_HEIGHT) {
+                    return this.stop()
+                } else {
+                    const { down, up } = this.lookup()
 
-                            const availableCount = MAX_PASSENGERS - this.passengers.length
-                            const call_ids = calls.slice(0, availableCount)
-                            if (call_ids.length > 0) {
-                                for (const call of call_ids)
-                                    this.waitingCalls.remove(call)
-
-                                result.call_ids = call_ids.map(d => d.id)
-                            } else {
-                                result.command = 'CLOSE'
-                            }
+                    if (this.status === 'UPWARD') {
+                        if (up > 0) {
+                            return this.keepGoing()
                         } else {
-                            result.command = 'CLOSE'
+                            return this.stop()
                         }
                     } else {
-                        result.command = 'CLOSE'
+                        if (down > 0) {
+                            return this.keepGoing()
+                        } else {
+                            return this.stop()
+                        }
                     }
                 }
             } break
         }
 
-        if (result.call_ids?.length == 0)
-            delete result.call_ids
-        return result
+        throw new Error('Undefined Behaviour')
     }
 
-    PassengerExitThisFloor() {
-        return this.passengers.reduce((arr: Call[], p: Call) => {
-            if (p.end === this.floor)
-                arr.push(p)
-
-            return arr
-        }, [])
+    private exitExists(): boolean {
+        return this.passengers.filter(d => d.end === this.floor).length > 0
     }
 
-    WaitingCallsEnterThisFloor() {
-        return [...this.waitingCalls.calls()].reduce((arr: Call[], p: Call) => {
-            if (p.start === this.floor)
-                arr.push(p)
-
-            return arr
-        }, [])
+    private enterExists(): boolean {
+        return [...this.waitingCalls.calls()].filter(d => d.start === this.floor).length > 0
     }
 
-    PassengerEnterOnThisFloorWithSameDirection(): Call[] {
-        const calls: Call[] = []
-        for (const call of this.waitingCalls.calls()) {
-            if (call.start === this.floor) {
-                const diff = call.end - call.start
-
-                switch (this.ElevatorDirections()) {
-                    case 'UP': {
-                        if (diff > 0) {
-                            calls.push(call)
-                            this.waitingCalls.remove(call)
-                        }
-                    } break
-
-                    case 'DOWN': {
-                        if (diff < 0) {
-                            calls.push(call)
-                            this.waitingCalls.remove(call)
-                        }
-                    } break
-                }
-            }
-        }
-
-        return calls
+    private empty(): boolean {
+        return this.passengers.length === 0
     }
 
-    directionCounter() {
+    private full(): boolean {
+        return this.passengers.length === MAX_PASSENGERS_CAPACITY
+    }
+
+    private getDirectionCounter() { // ??????????
         let up = 0, down = 0
-        for (const p of this.passengers) {
-            if (p.end - p.start > 0)
+        for (const call of this.passengers) {
+            const delta = call.end - call.start
+            if (delta > 0)
                 up++
             else
                 down++
         }
 
-        return { up, down }
-    }
-
-    callsLookUp(include: boolean) {
-        let up = 0, down = 0
-        for (const p of this.waitingCalls.calls()) {
-            if (p.start > this.floor)
-                up++
-            else if (p.start < this.floor)
-                down++
-            else if (include && p.start === this.floor) {
-                up++
-                down++
-            }
+        return {
+            up, down
         }
-
-        return { up, down }
     }
 
-    ElevatorDirections(): 'UP' | 'DOWN' | null {
+    private getDirection(): 'UP' | 'DOWN' | null {
         if (this.direction !== undefined)
             return this.direction
 
-        if (this.passengers.length > 0) {
-            const { up, down } = this.directionCounter()
+        let up = 0, down = 0
+        for (const call of this.passengers) {
+            const delta = call.end - call.start
+            if (delta > 0)
+                up++
+            else
+                down++
+        }
 
+        if (up > 0 || down > 0) {
             if (up > down)
                 this.direction = 'UP'
-            else
+            else if (up < down)
                 this.direction = 'DOWN'
+            else {
+                if (this.floor >= Math.floor(this.maxHeight / 2))
+                    this.direction = 'UP'
+                else
+                    this.direction = 'DOWN'
+            }
         } else {
             this.direction = null
         }
@@ -273,22 +214,114 @@ class Elevator implements iElevator {
         return this.direction
     }
 
-    getUpperLower(): { down: number, up: number } {
-        let down = 0, up = 0
-        for (const p of this.waitingCalls.calls()) {
-            if (p.start < this.floor)
-                down++
-            else if (p.start > this.floor)
+    private lookup() { // 현재 층을 제외한 태울 수 있는 사람들
+        let up = 0, down = 0
+        for (const call of this.waitingCalls.calls()) {
+            if (call.start > this.floor)
                 up++
+            else if (call.start < this.floor)
+                down++
         }
 
-        return { down, up }
+        return { up, down }
+    }
+
+    private up(): Command {
+        return {
+            command: 'UP',
+            elevator_id: this.id
+        }
+    }
+
+    private down(): Command {
+        return {
+            command: 'DOWN',
+            elevator_id: this.id
+        }
+    }
+
+    private keepGoing(): Command {
+        if (this.status === 'UPWARD') {
+            return {
+                command: 'UP',
+                elevator_id: this.id
+            }
+        }
+
+        if (this.status === 'DOWNWARD') {
+            return {
+                command: 'DOWN',
+                elevator_id: this.id
+            }
+        }
+
+        const direction = this.getDirection()
+        if (direction === null)
+            throw new Error()
+
+        return {
+            command: direction,
+            elevator_id: this.id
+        }
+    }
+
+    private stop(): Command {
+        return {
+            command: 'STOP',
+            elevator_id: this.id
+        }
+    }
+
+    private open(): Command {
+        return {
+            command: 'OPEN',
+            elevator_id: this.id
+        }
+    }
+
+    enter(): Command {
+        const availableCapacity = MAX_PASSENGERS_CAPACITY - this.passengers.length
+        let direction = this.getDirection()
+        let calls = [...this.waitingCalls.calls()].filter(d => d.start === this.floor)
+        if (direction === 'UP')
+            calls = calls.filter(d => d.end - d.start > 0)
+        else if (direction === 'DOWN')
+            calls = calls.filter(d => d.end - d.start < 0)
+        calls = calls.slice(0, availableCapacity)
+
+        if (calls.length === 0)
+            calls = [...this.waitingCalls.calls()].filter(d => d.start === this.floor).slice(0, availableCapacity)
+
+        calls.map(d => this.waitingCalls.remove(d))
+
+        return {
+            command: 'ENTER',
+            elevator_id: this.id,
+            call_ids: calls.map(d => d.id)
+        }
+    }
+
+    exit(): Command {
+        const calls = this.passengers.filter(d => d.end === this.floor)
+
+        return {
+            command: 'EXIT',
+            elevator_id: this.id,
+            call_ids: calls.map(d => d.id)
+        }
+    }
+
+    close(): Command {
+        return {
+            command: 'CLOSE',
+            elevator_id: this.id
+        }
     }
 }
 
 let PROBLEM_NUMBER = 1
 let MAX_HEIGHT = -1
-const ELEVATOR_COUNT = 2
+const ELEVATOR_COUNT = 1
 
 switch (PROBLEM_NUMBER) {
     case 0: {
@@ -302,31 +335,41 @@ switch (PROBLEM_NUMBER) {
 }
 
 async function loop() {
-    try {
-        while (true) {
-            // await new Promise(res => setTimeout(res, 500)) // 0.5초마다 실행
+    let handledCall = 0
+    let totalCommandCount = 0
+    while (true) {
+        // await new Promise(res => setTimeout(res, 500)) // 0.5초마다 실행
 
-            const { data, status } = await axios.get<Respond>('/oncalls')
+        const { data, status } = await axios.get<Respond>('/oncalls')
 
-            const { calls, elevators, is_end, timestamp, token } = data
-            const callDB = new CallDB(calls)
+        const { calls, elevators, is_end, timestamp, token } = data
+        const callDB = new CallDB(calls)
 
-            if (is_end)
-                break
+        if (is_end)
+            break
 
-            const commands: Command[] = []
+        const commands: Command[] = []
 
-            for (const { floor, id, passengers, status } of elevators) {
-                const command = new Elevator(id, floor, passengers, status, MAX_HEIGHT, callDB).calculate()
-                commands.push(command)
-            }
+        for (const { floor, id, passengers, status } of elevators) {
+            const command = new Elevator(id, floor, passengers, status, MAX_HEIGHT, callDB).calculate()
+            commands.push(command)
+        }
 
+        commands.map(c => {
+            if (c.command === 'EXIT')
+                handledCall += (c.call_ids?.length || 0)
+        })
+
+        try {
+            console.log(`handled_calls: ${handledCall}, totalCommands: ${totalCommandCount}`)
+            // console.log(`CMD: ${commands[0].command}, ids: ${commands[0].call_ids}`)
             const respond = await axios.post('/action', {
                 commands
             })
+            totalCommandCount += 1
+        } catch (err) {
+            console.error(err)
         }
-    } catch (err) {
-        console.error(err)
     }
 }
 
